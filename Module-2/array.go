70a5c4dcdd722806f5134dbb3461e48712e7c6c3
Sushant <sushantnadavade006@gmail.com>
2024-07-25 12:57:05 +0530

    new code is added

---
+++ Module-2/array.go
@@ -0,0 +1,15 @@
+// introduction to array
+// declaring arrays
+// operations on arrays
+
+// arrays in functions
+// arrays example
+// important points about arrays
+
+
+// arrays have fixed size
+// arrays are homogeneous 
+// arrays are value type in go
+// go uses zero=based indexing in arrays
+
+
